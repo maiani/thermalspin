Fixed error in the Monte Carlo algorithm. Accelerated the simulation using Numba jit compiler. Improved output computation and visualization.

analyze_data.py
@@ -16,22 +16,30 @@ from math_utils import sph2xyz
 
 
 def load_data(simname):
+    """
+    Load the data of a simulation
+    :param simname: name of the simulation
+    :return: snapshots, params, snapshots_t, e, m
+    """
     simdir = "./simulations/{0}/".format(simname)
 
     params_file = open(simdir + "params.json", "r")
     params = json.load(params_file)
-    delta_snapshots = params["delta_snp"]
 
     snapshots = np.load(simdir + "snapshots.npy")
-    snapshots_number = snapshots.shape[0]
 
+    results = np.load(simdir + "results.npy")
+
+    e = results[:, 0]
+    m = results[:, 1:4]
     # Build steps axis
     snapshots_t = np.load(simdir + "snapshots_t.npy")
 
-    return snapshots, params, snapshots_t
+    return snapshots, params, snapshots_t, e, m
 
 
-def compute_EM(snapshots, params, snapshots_t):
+# Function for computing quantities
+def compute_em(snapshots, params):
     """
     Compute energy and magnetization
     """
@@ -41,58 +49,89 @@ def compute_EM(snapshots, params, snapshots_t):
     T = params["T"]
     snapshots_number = snapshots.shape[0]
 
-    E = np.zeros(shape=(snapshots_number))
-    M = np.zeros(shape=(snapshots_number, 3))
+    energy = np.zeros(shape=snapshots_number)
+    magnetization = np.zeros(shape=(snapshots_number, 3))
 
     for i in np.arange(0, snapshots_number):
-        sys = HeisenbergSystem(snapshots[i, :, :, :, :], J=J, h=h, T=T)
-        E[i] = sys.energy
-        M[i, :] = sys.magnetization
+        sys = HeisenbergSystem(snapshots[i, :, :, :, :], J=J, h=h, temperature=T)
+        energy[i] = sys.energy
+        magnetization[i, :] = sys.magnetization
 
-    return E, M
-
-
-def plot_energy(snapshots_t, E):
-    """"
-    Plot energy
-    """
-    plt.figure()
-    plt.plot(snapshots_t, E)
-    plt.title("Energy")
+    return energy, magnetization
 
 
-def plot_magnetization(snapshots_t, M):
+def compute_statistics(e, m):
+    e_mean = np.mean(e)
+    e_rmsd = np.sqrt(np.var(e))
+    m_mean = np.mean(m, axis=0)
+    m_rmsd = np.sqrt(np.var(m, axis=0))
+
+    stats = dict(e_mean=e_mean, e_rmsd=e_rmsd, m_mean=m_mean, m_rmsd=m_rmsd)
+    return stats
+
+
+# Functions for plotting
+def plot_energy(snapshots_t, energy):
     """"
     Plot energy
     """
     fig = plt.figure()
     ax = fig.gca()
-    line1, = ax.plot(snapshots_t, M[:, 0], label='Mx')
-    line2, = ax.plot(snapshots_t, M[:, 1], label='My')
-    line3, = ax.plot(snapshots_t, M[:, 2], label='Mz')
-    ax.legend()
-    plt.legend()
+    line1, = ax.plot(snapshots_t, energy)
+    plt.xlabel("step")
+    plt.ylabel("Energy")
+    plt.title("Total energy")
+    return fig
 
+
+def plot_magnetization(snapshots_t, magnetization):
+    """"
+    Plot energy
+    """
+    fig = plt.figure()
+    ax = fig.gca()
+    line1, = ax.plot(snapshots_t, magnetization[:, 0], label='Mx')
+    line2, = ax.plot(snapshots_t, magnetization[:, 1], label='My')
+    line3, = ax.plot(snapshots_t, magnetization[:, 2], label='Mz')
+    ax.legend()
+    plt.xlabel("step")
+    plt.title("Magnetization")
+    return fig
+
+
+def plot_abs_magnetization(snapshots_t, magnetization):
+    """"
+    Plot energy
+    """
+    fig = plt.figure()
+    ax = fig.gca()
+    line1, = ax.plot(snapshots_t, np.abs(magnetization[:, 0]), label='|Mx|')
+    line2, = ax.plot(snapshots_t, np.abs(magnetization[:, 1]), label='|My|')
+    line3, = ax.plot(snapshots_t, np.abs(magnetization[:, 2]), label='|Mz|')
+    ax.legend()
+    plt.xlabel("step")
+    plt.title("Absolute magnetization")
+    return fig
 
 def plot_state(snapshots, n=-1):
     """
     Plot system state
     """
-    Nx = snapshots.shape[1]
-    Ny = snapshots.shape[2]
-    Nz = snapshots.shape[3]
+    nx = snapshots.shape[1]
+    ny = snapshots.shape[2]
+    nz = snapshots.shape[3]
 
-    x, y, z = np.meshgrid(np.arange(0, Nx),
-                          np.arange(0, Ny),
-                          np.arange(0, Nz))
+    x, y, z = np.meshgrid(np.arange(0, nx),
+                          np.arange(0, ny),
+                          np.arange(0, nz))
 
     state = snapshots[n, :, :, :, :]
 
-    u = np.zeros(shape=(Nx, Ny, Nz))
-    v = np.zeros(shape=(Nx, Ny, Nz))
-    w = np.zeros(shape=(Nx, Ny, Nz))
+    u = np.zeros(shape=(nx, ny, nz))
+    v = np.zeros(shape=(nx, ny, nz))
+    w = np.zeros(shape=(nx, ny, nz))
 
-    for i, j, k in np.ndindex(Nx, Ny, Nz):
+    for i, j, k in np.ndindex(nx, ny, nz):
         u[i, j, k], v[i, j, k], w[i, j, k] = sph2xyz(state[i, j, k, 0], state[i, j, k, 1])
 
     fig = plt.figure()

heisenberg.py
@@ -16,35 +16,39 @@ import numpy as np
 
 from heisenberg_simulation import HeisenbergSimulation
 from heisenberg_system import HeisenbergSystem
+from math_utils import rand_sph
 
 
-def run_test_simulation():
-    """
-    Run a test simulation with predefined parameters
-    """
-    np.random.seed(1)
-    test_params = dict(J=25, h=0, T=5, nsteps=2000000, delta_snp=500)
-    nx, ny, nz = (8, 8, 8)
-    init_simulation("test", nx, ny, nz, params=test_params)
-    run_simulation("test")
-
-
-def init_simulation(simname, nx, ny, nz, params=None):
+def init_simulation(simname, nx, ny, nz, params=None, theta_0=None, phi_0=None):
     """
     Generate a lattice of spins aligned upward z
+    :param simname: Name of the simulation
     :param nx: Number of x cells
     :param ny: Number of y cells
     :param nz: Number of z cells
     :param params: parameters of the simulation
+    :param phi_0:
+    :param theta_0:
     """
-    default_params = dict(J=10, h=0, T=1, nsteps=100000, delta_snp=1000)
+    default_params = dict(J=1, h=0, T=1, nsteps=5000000, delta_snp=2500)
 
     if not params:
         params = default_params
 
     simdir = "./simulations/" + simname + "/"
     shutil.rmtree(simdir, ignore_errors=True)
-    state = np.zeros(shape=(nx, ny, nz, 2))
+
+    if theta_0 is None:
+        state = np.zeros(shape=(nx, ny, nz, 2))
+        for i, j, k in np.ndindex(nx, ny, nz):
+            theta_r, phi_r = rand_sph()
+            state[i, j, k, 0] = theta_r
+            state[i, j, k, 1] = phi_r
+
+    else:
+        state = np.ones(shape=(nx, ny, nz, 2))
+        state[:, :, :, 0] = state[:, :, :, 0] * theta_0
+        state[:, :, :, 1] = state[:, :, :, 1] * phi_0
 
     os.makedirs(simdir)
     params_file = open(simdir + "params.json", "w")
@@ -81,10 +85,20 @@ def run_simulation(simname):
     run_time = end - start
     print("Simulation completed in {0} seconds".format(run_time))
 
+
     print("Saving results ...", end="")
-    if os.path.isfile(simdir + "snapshots.npy") and os.path.isfile(simdir + "snapshots_t.npy"):
+
+    results = np.zeros(shape=(hsim.snapshot_number, 4))
+    results[:, 0] = hsim.snapshots_e
+    results[:, 1:4] = hsim.snapshots_m
+
+    if os.path.isfile(simdir + "snapshots.npy") and os.path.isfile(simdir + "snapshots_t.npy") and os.path.isfile(
+            simdir + "results.npy"):
         old_snapshots = np.load(simdir + "snapshots.npy")
         snapshots = np.concatenate((old_snapshots, hsim.snapshots[1:]))
+
+        old_results = np.load(simdir + "results.npy")
+        results = np.concatenate((old_results, results[1:]))
 
         old_snapshots_t = np.load(simdir + "snapshots_t.npy")
         last_t = old_snapshots_t[-1]
@@ -97,23 +111,28 @@ def run_simulation(simname):
     np.save(simdir + "snapshots.npy", snapshots)
     np.save(simdir + "snapshots_t.npy", snapshots_t)
     np.save(simdir + "state.npy", hsim.system.state)
+    np.save(simdir + "results.npy", results)
     print("done")
 
 def usage():
     print("""
     Usage: heisenberg.py [OPTIONS] [PARAMETERS]\n
     -r, --run=SIMNAME                 Run a simulation named SIMNAME
-    -d, --default=SIZE                Generate a default simulation with SIZE specified e.g. 10x10x10
-    -t, --test                        Run a test simulations with default parameters to measure performances
+    -d, --dimensions=SIZE             Generate a default simulation with SIZE specified e.g. 10x10x10
+    -m, --magnetization=DIRECTION     Initial magnetization along DIRECTION specified like 0,0
     -h, --help                        Shows this message
     """)
 
 
 if __name__ == "__main__":
+    # Fallback
     mode = ""
+    simname = "default"
+    nx, ny, nz = (8, 8, 8)
+    theta_0, phi_0 = (None, None)
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "htr:i:d:", ["help", "test", "initialize=", "run=", "dimensions="])
+        opts, args = getopt.getopt(sys.argv[1:], "hr:i:d:m:", ["help", "initialize=", "run=", "dimensions="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -129,17 +148,23 @@ if __name__ == "__main__":
             simname = arg
         elif opt in ("-d", "--dimensions"):
             nx, ny, nz = arg.split("x")
-        elif opt in ("-t", "--test"):
-            mode = "test"
+        elif opt in ("-m", "--magnetization"):
+            theta_0, phi_0 = arg.split(",")
 
     if mode == "run":
         print(f"Running simulation {simname}")
         run_simulation(simname)
     elif mode == "init":
-        init_simulation(simname, int(nx), int(ny), int(nz))
-        print(f"Default simulation {simname} generated withe default params. \nLattice has dimensions {nx}x{ny}x{nz}")
-    elif mode == "test":
-        run_test_simulation()
+        if theta_0 is None:
+            init_simulation(simname, int(nx), int(ny), int(nz))
+            print(f"Default simulation {simname} generated withe default params. \n"
+                  f"Lattice has dimensions {nx}x{ny}x{nz} \n"
+                  f"Random initial magnetization")
+        else:
+            init_simulation(simname, int(nx), int(ny), int(nz), theta_0=int(theta_0), phi_0=int(phi_0))
+            print(f"Default simulation {simname} generated withe default params. \n"
+                  f"Lattice has dimensions {nx}x{ny}x{nz} \n"
+                  f"Initial magnetization ({theta_0},{phi_0})")
     else:
         usage()
         sys.exit(2)

heisenberg_simulation.py
@@ -30,8 +30,12 @@ class HeisenbergSimulation:
         self.snapshot_number = int(nsteps / snapshot_delta) + 1
         self.snapshots_t = np.arange(0, self.snapshot_number) * snapshot_delta
         self.snapshots = np.zeros(shape=(self.snapshot_number, self.system.nx, self.system.ny, self.system.nz, 2))
+        self.snapshots_e = np.zeros(shape=self.snapshot_number)
+        self.snapshots_m = np.zeros(shape=(self.snapshot_number, 3))
 
-        self.snapshots[0:, :, :, :, :] = hsys.state.copy()
+        self.snapshots[0:, :, :, :, :] = self.system.state.copy()
+        self.snapshots_e[0] = self.system.energy
+        self.snapshots_m[0, :] = self.system.magnetization
 
     def run(self):
 
@@ -41,3 +45,5 @@ class HeisenbergSimulation:
             if i != 0:
                 print("Step number {0} ".format(t))
                 self.snapshots[i, :, :, :, :] = self.system.state.copy()
+                self.snapshots_e[i] = self.system.energy
+                self.snapshots_m[i, :] = self.system.magnetization

heisenberg_system.py
@@ -4,32 +4,38 @@
 """
 Classical Heisenberg model Monte Carlo simulator
 """
-
 import numpy as np
+from numba import jit
 
 from math_utils import sph_dot, sph2xyz, rand_sph
 
 
 class HeisenbergSystem:
 
-    def __init__(self, state, J, h, T):
+    def __init__(self, state, J, h, temperature):
         self.J = J
         self.h = h
-        self.T = T
-        self.beta = 1 / T
+        self.temperature = temperature
+        self.beta = 1 / temperature
 
         self.state = state
         self.nx = self.state.shape[0]
         self.ny = self.state.shape[1]
         self.nz = self.state.shape[2]
+        self.nspin = self.nx * self.ny * self.nz
+        self.energy = self.compute_energy()
+        self.total_magnetization = self.compute_magnetization()
 
     @property
-    def energy(self):
+    def magnetization(self):
+        return self.total_magnetization / self.nspin
+
+    def compute_energy(self):
         """
         Compute the energy of the system
         :return: The value of the energy
         """
-        H = 0
+        energy_counter = 0
 
         dotp_counter = 0
         ext_field_counter = 0
@@ -55,95 +61,108 @@ class HeisenbergSystem:
                                         self.state[i, j, k, 1] - self.state[i, j, kk, 1])
                 kk = (k - 1) % self.nz
                 dotp_counter += sph_dot(self.state[i, j, k, 0], self.state[i, j, kk, 0],
-                                        self.state[i, j, k, 1] - self.state[ii, j, kk, 1])
+                                        self.state[i, j, k, 1] - self.state[i, j, kk, 1])
+
             ext_field_counter += np.cos(self.state[i, j, k, 0])
 
-        H += -self.J * dotp_counter
-        H += -self.h * ext_field_counter
+        energy_counter += -self.J * dotp_counter
+        energy_counter += -self.h * ext_field_counter
 
-        return H
+        return np.array(energy_counter)
 
-    @property
-    def magnetization(self):
+    def compute_magnetization(self):
         """
         Compute the mean magnetization
         :return: [Mx, My, Mz] vector of mean magnetization
         """
-        counter_x = 0
-        counter_y = 0
-        counter_z = 0
+        counter_r = np.zeros(3)
 
         for i, j, k in np.ndindex(self.nx, self.ny, self.nz):
-            x, y, z = sph2xyz(self.state[i, j, k, 0], self.state[i, j, k, 1])
-            counter_x += x
-            counter_y += y
-            counter_z += z
+            r = sph2xyz(self.state[i, j, k, 0], self.state[i, j, k, 1])
+            counter_r += r
 
-        return np.array([counter_x, counter_y, counter_z]) / self.nx / self.ny / self.nz
+        return counter_r
+
 
     def step(self):
         """
         Evolve the system computing a step of Metropolis-Hastings Monte Carlo
         """
+        s, e, m = numba_step(self.state, self.nx, self.ny, self.nz, self.J, self.h, self.beta, self.energy,
+                             self.total_magnetization)
+        self.state = s
+        self.energy = e
+        self.total_magnetization = m
 
-        i = np.random.randint(0, self.nx)
-        j = np.random.randint(0, self.ny)
-        k = np.random.randint(0, self.nz)
 
-        r_theta, r_phi = rand_sph()
+@jit(nopython=True)
+def numba_step(state, nx, ny, nz, J, h, beta, energy, total_magnetization):
+    # Select a random spin in the system
+    i = np.random.randint(0, nx)
+    j = np.random.randint(0, ny)
+    k = np.random.randint(0, nz)
 
-        E0 = 0
-        ii = (i + 1) % self.nx
-        E0 += sph_dot(self.state[i, j, k, 0], self.state[ii, j, k, 0],
-                      self.state[i, j, k, 1] - self.state[ii, j, k, 1])
-        ii = (i - 1) % self.nx
-        E0 += sph_dot(self.state[i, j, k, 0], self.state[ii, j, k, 0],
-                      self.state[i, j, k, 1] - self.state[ii, j, k, 1])
-        jj = (j + 1) % self.ny
-        E0 += sph_dot(self.state[i, j, k, 0], self.state[i, jj, k, 0],
-                      self.state[i, j, k, 1] - self.state[i, jj, k, 1])
-        jj = (j - 1) % self.ny
-        E0 += sph_dot(self.state[i, j, k, 0], self.state[i, jj, k, 0],
-                      self.state[i, j, k, 1] - self.state[i, jj, k, 1])
+    # Compute the energy due to that spin
+    e0 = 0
+    ii = (i + 1) % nx
+    e0 += sph_dot(state[i, j, k, 0], state[ii, j, k, 0],
+                  state[i, j, k, 1] - state[ii, j, k, 1])
+    ii = (i - 1) % nx
+    e0 += sph_dot(state[i, j, k, 0], state[ii, j, k, 0],
+                  state[i, j, k, 1] - state[ii, j, k, 1])
+    jj = (j + 1) % ny
+    e0 += sph_dot(state[i, j, k, 0], state[i, jj, k, 0],
+                  state[i, j, k, 1] - state[i, jj, k, 1])
+    jj = (j - 1) % ny
+    e0 += sph_dot(state[i, j, k, 0], state[i, jj, k, 0],
+                  state[i, j, k, 1] - state[i, jj, k, 1])
 
-        if self.nz > 1:
-            kk = (k + 1) % self.nz
-            E0 += sph_dot(self.state[i, j, k, 0], self.state[i, j, kk, 0],
-                          self.state[i, j, k, 1] - self.state[i, j, kk, 1])
-            kk = (k - 1) % self.nz
-            E0 += sph_dot(self.state[i, j, k, 0], self.state[i, j, kk, 0],
-                          self.state[i, j, k, 1] - self.state[ii, j, kk, 1])
+    if nz > 1:
+        kk = (k + 1) % nz
+        e0 += sph_dot(state[i, j, k, 0], state[i, j, kk, 0],
+                      state[i, j, k, 1] - state[i, j, kk, 1])
+        kk = (k - 1) % nz
+        e0 += sph_dot(state[i, j, k, 0], state[i, j, kk, 0],
+                      state[i, j, k, 1] - state[i, j, kk, 1])
 
-        E0 *= -self.J
-        E0 += -self.h * np.cos(self.state[i, j, k, 0])
+    e0 *= -2 * J
+    e0 += -h * np.cos(state[i, j, k, 0])
 
-        E1 = 0
-        ii = (i + 1) % self.nx
-        E1 += sph_dot(r_theta, self.state[ii, j, k, 0],
-                      r_phi - self.state[ii, j, k, 1])
-        ii = (i - 1) % self.nx
-        E1 += sph_dot(r_theta, self.state[ii, j, k, 0],
-                      r_phi - self.state[ii, j, k, 1])
-        jj = (j + 1) % self.ny
-        E1 += sph_dot(r_theta, self.state[i, jj, k, 0],
-                      r_phi - self.state[i, jj, k, 1])
-        jj = (j - 1) % self.ny
-        E1 += sph_dot(self.state[i, j, k, 0], self.state[i, jj, k, 0],
-                      r_phi - self.state[i, jj, k, 1])
+    # Generate a new random direction and compute energy due to the spin in the new direction
+    r_theta, r_phi = rand_sph()
 
-        if self.nz > 1:
-            kk = (k + 1) % self.nz
-            E1 += sph_dot(r_theta, self.state[i, j, kk, 0],
-                          r_phi - self.state[i, j, kk, 1])
-            kk = (k - 1) % self.nz
-            E1 += sph_dot(r_theta, self.state[i, j, kk, 0],
-                          r_phi - self.state[ii, j, kk, 1])
+    e1 = 0
+    ii = (i + 1) % nx
+    e1 += sph_dot(r_theta, state[ii, j, k, 0],
+                  r_phi - state[ii, j, k, 1])
+    ii = (i - 1) % nx
+    e1 += sph_dot(r_theta, state[ii, j, k, 0],
+                  r_phi - state[ii, j, k, 1])
+    jj = (j + 1) % ny
+    e1 += sph_dot(r_theta, state[i, jj, k, 0],
+                  r_phi - state[i, jj, k, 1])
+    jj = (j - 1) % ny
+    e1 += sph_dot(r_theta, state[i, jj, k, 0],
+                  r_phi - state[i, jj, k, 1])
 
-        E1 *= -self.J
-        E1 += -self.h * np.cos(r_theta)
+    if nz > 1:
+        kk = (k + 1) % nz
+        e1 += sph_dot(r_theta, state[i, j, kk, 0],
+                      r_phi - state[i, j, kk, 1])
+        kk = (k - 1) % nz
+        e1 += sph_dot(r_theta, state[i, j, kk, 0],
+                      r_phi - state[i, j, kk, 1])
 
-        w = np.exp(self.beta * (E0 - E1))
-        dice = np.random.uniform()
+    e1 *= -2 * J
+    e1 += -h * np.cos(r_theta)
 
-        if dice < w:
-            self.state[i, j, k, :] = np.array([r_theta, r_phi])
+    # Apply Metropolis algorithm
+    w = np.exp(beta * (e0 - e1))
+    dice = np.random.uniform(0, 1)
+
+    if dice < w:
+        energy += (e1 - e0)
+        total_magnetization += (sph2xyz(r_theta, r_phi) - sph2xyz(state[i, j, k, 0], state[i, j, k, 1]))
+        state[i, j, k, :] = np.array([r_theta, r_phi])
+
+    return state, energy, total_magnetization

math_utils.py
@@ -5,8 +5,10 @@ Mathematical utility functions
 """
 
 import numpy as np
+from numba import jit
 
 
+@jit(nopython=True)
 def sph2xyz(theta, phi):
     """
     Convert spherical coordinates to unit vector
@@ -17,9 +19,10 @@ def sph2xyz(theta, phi):
     x = np.sin(theta) * np.cos(phi)
     y = np.sin(theta) * np.sin(phi)
     z = np.cos(theta)
-    return x, y, z
+    return np.array([x, y, z])
 
 
+@jit(nopython=True)
 def sph_dot(theta1, theta2, delta_phi):
     """
     Compute the dot product of two unit vectors in spherical coordinates
@@ -27,37 +30,16 @@ def sph_dot(theta1, theta2, delta_phi):
     return np.sin(theta1) * np.cos(delta_phi) * np.sin(theta2) + np.cos(theta1) * np.cos(theta2)
 
 
+@jit(nopython=True)
 def rand_sph():
     """
     Generate random unit vector in spherical coordinates
     :return: (theta, phi) the two angles
     """
-    u, v = np.random.uniform(size=(2))
+
+    phi = np.random.uniform(0, 2 * np.pi)
+    u = np.random.uniform(0, 1)
     theta = np.arccos(2 * u - 1)
-    phi = 2 * np.pi * v
 
     return theta, phi
-
-
-def sph_dot_lookup_table(ntheta, nphi):
-    """
-    Build lookup table for dot product of two unit vectors in spherical coordinates
-    :param ntheta: number of theta points
-    :param nphi:   number of phi point
-    :return: (tab, theta, phi) arrays
-    """
-    tab = np.zeros(shape=(ntheta, ntheta, nphi))
-
-    theta = np.linspace(0, np.pi, ntheta)
-    phi = np.linspace(0, np.pi, nphi)
-
-    for i, ii, j in np.ndindex(ntheta, ntheta, nphi):
-        tab[i, ii, j] = sph_dot(theta[i], theta[ii], phi[j])
-
-    return tab, theta, phi
-
-
-def sph_dot_lotab(ntheta1, ntheta2, ndelta_phi, tab):
-    pass
-
 